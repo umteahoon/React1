9accb34eca1e2ca7cfda4677803a7cdcb33fc3a8
umteahoon <freeam2006@gmail.com>
2026-04-08 11:22:17 +0900

    0408

--- react0408/src/components/Items.jsx
+++ react0408/src/components/Items.jsx
@@ -1,6 +1,26 @@
+// export default function Items({ name, isPacked }) {
+//   if (isPacked) {
+//     return <li>{name} ✅</li>;
+//   }
+//   return <li>{name}</li>;
+// }
+
+// Step 7: Items.jsx
 export default function Items({ name, isPacked }) {
-  if (isPacked) {
-    return <li>{name} ✅</li>;
-  }
-  return <li>{name}</li>;
+  return <li>{name} {isPacked ? "✅" : ""}</li>;
 }
+
+// Step 8: Items.jsx
+// export default function Items({ name, isPacked }) {
+//   return (
+//     <li>
+//       {isPacked ? (
+//         <del>
+//           {name + " ✅"}
+//         </del>
+//       ) : (
+//         name
+//       )}
+//     </li>
+//   )
+// }
