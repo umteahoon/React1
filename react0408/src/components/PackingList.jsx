9b0340357307eb4002783b906c4c1be248a2cd1c
umteahoon <freeam2006@gmail.com>
2026-04-08 11:09:29 +0900

    0408

--- react0408/src/components/PackingList.jsx
+++ react0408/src/components/PackingList.jsx
@@ -6,7 +6,7 @@ export default function PackingList () {
       <section>
         <h1>여행 짐 리스트</h1>
         <ul>
-          <Items name="여분 옷" isPacked={true} />
+          <Items name="여분 옷" isPacked={false} />
           <Items name="노트북" isPacked={false} />
           <Items name="컵라면" isPacked={true} />
         </ul>
