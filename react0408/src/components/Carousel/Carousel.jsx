8e595ed2b6ef1d1da1ec470df097c31c8d2b8d0c
umtahoon <am2869@naver.com>
2026-05-27 10:50:09 +0900

    0527

--- react0408/src/components/Carousel/Carousel.jsx
+++ react0408/src/components/Carousel/Carousel.jsx
@@ -4,6 +4,8 @@ import styles from "./Carousel.module.css";
 
 export default function Carousel() {
   const [index, setIndex] = useState(0);
+  // 1. 블리언(Boolean) 타입의 State를 추가합니다. 변수의 이름은 more로 합니다.
+  const [more, setMore] = useState(false);
 
   function handleNext() {
     if (index === galleryImages.length - 1) {
@@ -21,12 +23,17 @@ export default function Carousel() {
     }
   }
 
+  // 2. 토글할 수 있는 handleMoreClick 이벤트 핸들러를 작성합니다.
+  function handleMoreClick() {
+    setMore(!more);
+  }
+
   let slide = galleryImages[index];
   return (
     <section className={styles.wrapper}>
       <p>
-        <button onClick={handlePrevious}>Previous</button>
-        <button onClick={handleNext}>Next</button>
+        <button className={styles.button} onClick={handlePrevious}>Previous</button>
+        <button className={styles.button} onClick={handleNext}>Next</button>
       </p>
       <h2>
         <i>{slide.name} </i>
@@ -35,8 +42,16 @@ export default function Carousel() {
       <h3>
         ({index + 1} of {galleryImages.length})
       </h3>
+      
+      {/* 3, 4. 버튼을 추가하고 클릭 이벤트 연결 및 more 값에 따른 라벨 변경 */}
+      <button className={styles.button} onClick={handleMoreClick}>
+        {more ? "Hide description" : "Show description"}
+      </button>
+
       <img src={slide.url} alt={slide.alt} />
-      <p>{slide.description}</p>
+      
+      {/* 5. More가 true면 description을 보여주고, false면 보이지 않게 합니다. (조건부 렌더링) */}
+      {more && <p>{slide.description}</p>}
     </section>
   );
 }
